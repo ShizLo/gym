87a60aedc58f41b188f476412b91c7ca9b870517
ShizLo <pavelshizo@mail.ru>
2023-01-10 16:38:49 +0300

    gulp

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,42 +1,34 @@
 import fileinclude from "gulp-file-include"
-import webpHtmlNosvg from 'gulp-webp-html-nosvg'
-import versionNumber from 'gulp-version-number' //Убираем кеширование
+import webpHtmlNosvg from "gulp-webp-html-nosvg"
+import versionNumber from "gulp-version-number" //Убираем кеширование
 
 export const html = () => {
-  return app.gulp.src(app.path.src.html)
-  .pipe(app.plugins.plumber(
-    app.plugins.notify.onError({
-      title: "HTML",
-      message: "Error: <%= error.message %>"
-    })
-  ))
-  .pipe(fileinclude())
-  .pipe(app.plugins.replace(/@img\//g, 'img/'))
-  .pipe(
-    app.plugins.if(
-      app.isBuild,
-      webpHtmlNosvg()
+  return app.gulp
+    .src(app.path.src.html)
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "HTML",
+          message: "Error: <%= error.message %>",
+        })
+      )
     )
-  )
-  .pipe(
-    app.plugins.if(
-      app.isBuild,
+    .pipe(fileinclude())
+    .pipe(app.plugins.replace(/@img\//g, "img/"))
+    .pipe(webpHtmlNosvg())
+    .pipe(
       versionNumber({
-        'value': '%DT%',
-        'append': {
-          'key': '_v',
-          'cover': 0,
-          'to': [
-            'css',
-            'js'
-          ]
+        value: "%DT%",
+        append: {
+          key: "_v",
+          cover: 0,
+          to: ["css", "js"],
         },
-        'output': {
-          'file': 'gulp/version.json'
-        }
+        output: {
+          file: "gulp/version.json",
+        },
       })
     )
-  )
-  .pipe(app.gulp.dest(app.path.build.html))
-  .pipe(app.plugins.browsersync.stream())
+    .pipe(app.gulp.dest(app.path.build.html))
+    .pipe(app.plugins.browsersync.stream())
 }
